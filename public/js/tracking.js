e3ae01ab71972e8b023cdcb14ba6245265307aae
Alexis Tyler <OmgImAlexis@gmail.com>
2015-03-01 20:31:33 +1030

    GUID Cookie added

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,3 +1,6 @@
+// FROM THIS PAGE!
+// http://clubmate.fi/setting-and-reading-cookies-with-javascript/
+function createCookie(e,t,n){var o;if(n){var i=new Date;i.setTime(i.getTime()+24*n*60*60*1e3),o="; expires="+i.toGMTString()}else o="";document.cookie=e+"="+t+o+"; path=/"}function readCookie(e){for(var t=e+"=",n=document.cookie.split(";"),o=0;o<n.length;o++){for(var i=n[o];" "===i.charAt(0);)i=i.substring(1,i.length);if(0===i.indexOf(t))return i.substring(t.length,i.length)}return null}function eraseCookie(e){createCookie(e,"",-1)}
 TrkTracking = {
   track: function(env) {
     if(typeof(env) == "undefined") { env = {}; }
@@ -7,14 +10,14 @@ TrkTracking = {
     env.width = window.innerWidth;
     env.height = window.innerHeight;
 
-    // example of sending a cookie named 'guid'
-    // env.guid = (document.cookie.match(/guid=([^\_]*)_([^;]*)/) || [])[2];
-
     if(document.referrer && document.referrer != "") {
       env.ref = document.referrer;
     }
 
-    env.rnd = Math.floor(Math.random() * 10e12);
+    if(!readCookie('trk_guid')){
+        createCookie('trk_guid', (Math.floor(Math.random() * 10e12)), 30);
+    }
+    env.guid = readCookie('trk_guid');
 
     var params = [];
     for(var key in env) {
