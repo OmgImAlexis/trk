fc4b6fa7a8fbb5f9b77ab919db8dd5252faa056c
Alexis Tyler <OmgImAlexis@gmail.com>
2015-03-02 12:08:14 +1030

    Added other console messages about DNT

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,6 +1,8 @@
+console.log('As we are in beta all metrics can be viewed at https://trk.wvvw.me/metrics');
 if (navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1") {
-    console.log('You don\'t want to be tracked so TrkTracking has been disabled for you. Checkout http://trk.wvvw.me to learn more.');
+    console.log('You don\'t want to be tracked so TrkTracking has been disabled for you.');
 } else {
+    console.log('Currently TrkTracking is tracking you, if you would like us not to then please enable the DNT option in your browser.');
     // FROM THIS PAGE!
     // http://clubmate.fi/setting-and-reading-cookies-with-javascript/
     function createCookie(e,t,n){var o;if(n){var i=new Date;i.setTime(i.getTime()+24*n*60*60*1e3),o="; expires="+i.toGMTString()}else o="";document.cookie=e+"="+t+o+"; path=/"}function readCookie(e){for(var t=e+"=",n=document.cookie.split(";"),o=0;o<n.length;o++){for(var i=n[o];" "===i.charAt(0);)i=i.substring(1,i.length);if(0===i.indexOf(t))return i.substring(t.length,i.length)}return null}function eraseCookie(e){createCookie(e,"",-1)}
@@ -35,3 +37,4 @@ if (navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.ms
     };
     TrkTracking.track();
 }
+console.log('Checkout https://trk.wvvw.me to learn more.');
