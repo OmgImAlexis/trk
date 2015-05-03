227f9bbdfbea4a9d074daa11c84a4521988d8b14
Alexis Tyler <OmgImAlexis@gmail.com>
2015-05-03 14:21:03 +0930

    Changed positioning

--- public/js/viewcounter.js
+++ public/js/viewcounter.js
@@ -1,15 +1,19 @@
 window.onload = function() {
-    $.getScript('/api/read/json', function(){
-        var blog_url = tumblr_api_read.tumblelog.name;
-        $.ajax({
-            url: 'https://trk.wvvw.me/blog/' + blog_url + '/hits?json=true',
-            dataType: 'jsonp',
-            success: function(data){
-                var a = document.createElement('a');
-                a.href = 'https://trk.wvvw.me/blog/' + blog_url;
-                a.text = data.hits + ' Visits';
-                document.getElementsByTagName('body')[0].appendChild(a);
-            }
+    if ($ === jQuery) {
+        $.getScript('/api/read/json', function(){
+            var blog_url = tumblr_api_read.tumblelog.name;
+            $.ajax({
+                url: 'https://trk.wvvw.me/blog/' + blog_url + '/hits?json=true',
+                dataType: 'jsonp',
+                success: function(data){
+                    var a = document.createElement('a');
+                    a.href = 'https://trk.wvvw.me/blog/' + blog_url;
+                    a.text = data.hits + ' Visits';
+                    a.className = 'btn';
+                    a.style.cssText = 'position:fixed;top:28px;right:3px;';
+                    document.getElementsByTagName('body')[0].appendChild(a);
+                }
+            });
         });
-    });
+    }
 };
